a13ba25e965e850e0460fee141594545c3524467
vpnineh <fffarbod@bk.ru>
2024-12-12 23:56:54 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,25 +1,7 @@
 <?php
 
 $subscription_urls = [
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/vmess_iran.txt",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
   "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/trojan_iran.txt",
-  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/vless_iran.txt",
-  "https://igdux.top/~sub-tnetplus",
-  "https://links.achaemenidempireofpersia.uk/cR3Q5X#IrancpinetSub1",
-  "https://sub1.achaemenidempireofpersia.uk/#IrancpinetSub2",
-  "https://raw.githubusercontent.com/jahangirGH/sublinks/main/Fragsubhiddify.txt#Irancpi_Hiddify_Fragment",
-  "https://raw.githubusercontent.com/Surfboardv2ray/Subs/main/Raw",
-  "https://raw.githubusercontent.com/Surfboardv2ray/Subs/main/Realm",
   "https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
-  "https://msdonor.pages.dev/sub/7f035a95-4b51-4e0b-8995-59864a9cde22#CryptoGuardVPN",
-  "https://msdonor.pages.dev/sub/7f035a95-4b51-4e0b-8995-59864a9cde22#BPB-Normal",
-  "https://raw.githubusercontent.com/xc0000e9/deatnote/main/Hiddify-next.fragment",
-  "https://hiddify-freevpnhomes-subscription.meshkintaj.homes",
-  "https://fragment-freevpnhomes-subscription.meshkintaj.homes",
-  "https://sub.xornot.ir/api/prossh.php",
-  "https://raw.githubusercontent.com/proSSHvpn/proSSHvpn/main/sub",
-  "http://103.196.20.127/api/v1/client/subscribe?token=ba1205a8193bdf364fc70eb5bd5efb0d#subscribe?token=ba1205a8193bdf364fc70eb5bd5efb0d",
-  "https://raw.githubusercontent.com/iPsycho1/Subscription/refs/heads/main/iPsycho-Fastly",
-
 ];
