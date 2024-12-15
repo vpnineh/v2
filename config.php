75d340a692239ec99afbcbd099b177bde61d5601
vpnineh <fffarbod@bk.ru>
2024-12-16 01:22:56 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 $subscription_urls = [
-  #"https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
   #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
   #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/ss_iran.txt",
   #"https://raw.githubusercontent.com/coldwater-10/V2ray-Config-Lite/refs/heads/main/Splitted-By-Protocol/ss.txt",
