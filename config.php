f7ef78f477b9cbae0ab553f86a2f40e81f91495c
vpnineh <fffarbod@bk.ru>
2024-12-25 23:06:48 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
 
 $subscription_urls = [
-  "https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
-  "https://raw.githubusercontent.com/coldwater-10/V2RayAggregator/refs/heads/master/Eternity.txt",
-];
-  #"https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
+  #"https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
+  #"https://raw.githubusercontent.com/coldwater-10/V2RayAggregator/refs/heads/master/Eternity.txt",
+  "https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
   #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorLire/main/ss_iran.txt",
-  #"https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/ss_iran.txt",
+  "https://raw.githubusercontent.com/coldwater-10/V2rayCollectorVpnclashfa/main/ss_iran.txt",
   #"https://raw.githubusercontent.com/coldwater-10/V2ray-Config-Lite/refs/heads/main/Splitted-By-Protocol/ss.txt",
+
+];
