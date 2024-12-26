36146af504c2c7f21157b18660ae22d2eea3f2b9
vpnineh <fffarbod@bk.ru>
2024-12-26 18:28:47 +0330

    Update index.php

--- index.php
+++ index.php
@@ -49,4 +49,8 @@ file_put_contents("Split/Base64/shadowsocks", base64_encode($merged_shadowsocks)
 file_put_contents("data.txt", file_get_contents("https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt"));
 }
 else
-{exit(0);}
+{
+  echo "Tekrari. \n";
+  exit(0);
+}
+  exit(0);
