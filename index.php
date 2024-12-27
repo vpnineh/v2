dd7f131eee95f34c274d9f1301d1ea592d625f73
vpnineh <fffarbod@bk.ru>
2024-12-27 06:40:41 +0330

    Update index.php

--- index.php
+++ index.php
@@ -59,6 +59,3 @@ file_put_contents("Split/Normal/shadowsocks", $merged_shadowsocks);
 file_put_contents("Split/Base64/shadowsocks", base64_encode($merged_shadowsocks));
 file_put_contents("data.txt", file_get_contents("https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt"));
 }
-else{
-   return 0;
-}
