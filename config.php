cce45c89eddef7930d22fd408ee8807d5416ea8d
vpnineh <fffarbod@bk.ru>
2024-12-29 02:48:33 +0330

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 
 $subscription_urls = [
+  "https://github.com/vpnineh/sub/raw/refs/heads/main/hub",
   #"https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt",
   #"https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
-  "https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
-  "https://raw.githubusercontent.com/vpnineh/sub/refs/heads/main/go",
+  #"https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
+  #"https://raw.githubusercontent.com/vpnineh/sub/refs/heads/main/go",
   #"https://raw.githubusercontent.com/coldwater-10/V2RayAggregator/refs/heads/master/Eternity.txt",
   #"https://raw.githubusercontent.com/Surfboardv2ray/Proxy-sorter/main/output/converted.txt",
   #"https://raw.githubusercontent.com/coldwater-10/V2rayCollector_mahsaserver/main/ss_iran.txt",
