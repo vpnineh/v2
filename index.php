60e27914d3902459ad35e6dec1ad459c9124f8c8
vpnineh <fffarbod@bk.ru>
2025-04-10 23:34:21 +0330

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include "config.php";
 include "functions.php";
 
 $data = file_get_contents("data.txt");
-$data1 = file_get_contents("https://github.com/iPsycho1/Subscription/raw/refs/heads/main/iPsycho_Test-Config");
+$data1 = file_get_contents("https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt");
 
 
 
@@ -54,7 +54,7 @@ file_put_contents("Split/Normal/trojan", $merged_trojan);
 file_put_contents("Split/Base64/trojan", base64_encode($merged_trojan));
 file_put_contents("Split/Normal/shadowsocks", $merged_shadowsocks);
 file_put_contents("Split/Base64/shadowsocks", base64_encode($merged_shadowsocks));
-file_put_contents("data.txt", file_get_contents("https://github.com/iPsycho1/Subscription/raw/refs/heads/main/iPsycho_Test-Config"));
+file_put_contents("data.txt", file_get_contents("https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt"));
 
 exit(0);
 } else {
