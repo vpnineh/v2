05cd3da760ea9a22d61baea88aeb493e7f0ef4ca
vpnineh <fffarbod@bk.ru>
2026-01-29 12:38:31 +0330

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 $subscription_urls = [
   #"https://github.com/vpnineh/sub/raw/refs/heads/main/hub",
-  #"https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt",
+  "https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt",
   #"https://raw.githubusercontent.com/Kwinshadow/TelegramV2rayCollector/main/sublinks/mix.txt",
   #"https://raw.githubusercontent.com/Surfboardv2ray/v2ray-worker-sub/refs/heads/master/Eternity.txt",
   #"https://raw.githubusercontent.com/vpnineh/sub/refs/heads/main/go",
@@ -14,5 +14,6 @@ $subscription_urls = [
   #"https://raw.githubusercontent.com/coldwater-10/V2ray-Config-Lite/refs/heads/main/Splitted-By-Protocol/ss.txt",
   #"https://github.com/Surfboardv2ray/Proxy-sorter/raw/refs/heads/main/custom/mahsa.txt",
   #"https://raw.githubusercontent.com/soroushmirzaei/telegram-configs-collector/refs/heads/main/countries/ir/mixed",
-   "https://drive.google.com/uc?export=download&id=1-EopH8hKLwaRJ3kxm3-40x4CZQ3prAzP"
+  #"https://drive.google.com/uc?export=download&id=1-EopH8hKLwaRJ3kxm3-40x4CZQ3prAzP",
+  https://raw.githubusercontent.com/sakha1370/OpenRay/refs/heads/main/output_iran/iran_top100_checked.txt
 ];
